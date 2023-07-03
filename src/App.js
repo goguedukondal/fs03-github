541646ded8ff8855d7d354ae0dc1d7e41e3377c4
GOGU EDUKONDAL <97788201+goguedukondal@users.noreply.github.com>
2023-07-03 12:11:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 
 import './App.css';
-// import Comp from './Components/Comp';
-// import User from './Components/JSON_SERVER/User';
-// import Pagination from './Pagination/Pagination';
-// import A from './Components/Context/A';
-// import Parent from './Components/Context/Parent';
-// import Nav from './Components/Nav';
-// import Login from './Components/Login';
-// import Home from './Components/Home';
-// import { useState } from 'react';
+
 import GithubHome from './Githubcomp/GithubHome';
 function App() {
-//   const [isLoggedIn,setIsLoggedIn] = useState(false)
 
-//   const login = ()=>{
-//  setIsLoggedIn(true)
-//   }
-//   const logout = ()=>[
-//     setIsLoggedIn(false)
-//   ]
   return (
     <div className="App">
-      {/* <Comp/> */}
-      {/* <User/> */}
-      {/* <Pagination/> */}
-      {/* <A/> */}
-      {/* <Parent/> */}
-{/* <Nav logout={logout}/>
-     {
-      isLoggedIn?<Home/>:<Login login={login}/>
       
-     } */}
+
      <GithubHome/>
     </div>
   );
